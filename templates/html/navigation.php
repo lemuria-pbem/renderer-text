16e848b02464c1ebde92eb36ba9dd7db28ac1691
lemuria-pbem <lemuria@online.de>
2022-08-14 10:11:02 +0200

    [FEATURE] Show only regions with units in navigation.

--- templates/html/navigation.php
+++ templates/html/navigation.php
@@ -9,6 +9,7 @@ use Lemuria\Renderer\Text\View\Html;
 /** @var Html $this */
 
 $party     = $this->party;
+$census    = $this->census->getAtlas();
 $travelLog = $this->travelLog;
 
 ?>
@@ -20,21 +21,23 @@ $travelLog = $this->travelLog;
 				<?php if ($atlas->count() > 0): ?>
 					<a class="navbar-brand" href="#continent-<?= $continent->Id() ?>"><?= $continent->Name() ?></a>
 					<?php foreach ($atlas as $region /* @var Region $region */): ?>
-						<?php if ($region->Landscape() instanceof Ocean): ?>
-							<?php foreach (View::sortedFleet($region) as $vessel): ?>
-								<a class="vessel nav-link pb-0" href="#vessel-<?= $vessel->Id()->Id() ?>"><?= $vessel->Name() ?></a>
-							<?php endforeach ?>
-						<?php else: ?>
-							<a class="region nav-link" href="#region-<?= $region->Id()->Id() ?>"><?= $region->Name() ?></a>
-							<?php if ($region->Estate()->count() > 0 || $region->Fleet()->count() > 0): ?>
-								<nav id="nav-region-<?= $region->Id()->Id() ?>" class="nav nav-pills flex-column">
-									<?php foreach (View::sortedEstate($region) as $construction): ?>
-										<a class="construction nav-link ml-3 py-0" href="#construction-<?= $construction->Id()->Id() ?>"><?= $construction->Name() ?></a>
-									<?php endforeach ?>
-									<?php foreach (View::sortedFleet($region) as $vessel): ?>
-										<a class="vessel nav-link ml-3 py-0" href="#vessel-<?= $vessel->Id()->Id() ?>"><?= $vessel->Name() ?></a>
-									<?php endforeach ?>
-								</nav>
+						<?php if ($census->has($region->Id())): ?>
+							<?php if ($region->Landscape() instanceof Ocean): ?>
+								<?php foreach (View::sortedFleet($region) as $vessel): ?>
+									<a class="vessel nav-link pb-0" href="#vessel-<?= $vessel->Id()->Id() ?>"><?= $vessel->Name() ?></a>
+								<?php endforeach ?>
+							<?php else: ?>
+								<a class="region nav-link" href="#region-<?= $region->Id()->Id() ?>"><?= $region->Name() ?></a>
+								<?php if ($region->Estate()->count() > 0 || $region->Fleet()->count() > 0): ?>
+									<nav id="nav-region-<?= $region->Id()->Id() ?>" class="nav nav-pills flex-column">
+										<?php foreach (View::sortedEstate($region) as $construction): ?>
+											<a class="construction nav-link ml-3 py-0" href="#construction-<?= $construction->Id()->Id() ?>"><?= $construction->Name() ?></a>
+										<?php endforeach ?>
+										<?php foreach (View::sortedFleet($region) as $vessel): ?>
+											<a class="vessel nav-link ml-3 py-0" href="#vessel-<?= $vessel->Id()->Id() ?>"><?= $vessel->Name() ?></a>
+										<?php endforeach ?>
+									</nav>
+								<?php endif ?>
 							<?php endif ?>
 						<?php endif ?>
 					<?php endforeach ?>
