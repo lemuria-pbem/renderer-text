bd4404e216060ca67b1748d03f3a7c55e4385947
lemuria-pbem <lemuria@online.de>
2023-01-07 10:07:18 +0100

    [BUGFIX] Fix missing own units after the first in HTML report.

--- templates/html/apparitions/with-unit.php
+++ templates/html/apparitions/with-unit.php
@@ -10,22 +10,22 @@ use Lemuria\SortMode;
 /** @var Html $this */
 
 /** @var Region $region */
-$region         = $this->variables[0];
-$outlook        = $this->outlook;
-$apparitions    = $outlook->getApparitions($region)->sort(SortMode::ByParty, $this->party);
-$unitsInRegions = 0;
+$region      = $this->variables[0];
+$outlook     = $this->outlook;
+$apparitions = $outlook->getApparitions($region)->sort(SortMode::ByParty, $this->party);
+$i           = 0;
 
 ?>
-<?php foreach ($apparitions as $unit /* @var Unit $unit */): ?>
-	<?php if ($unitsInRegions++ === 0): ?>
-		<h5>Einheiten in der Region</h5>
-		<br>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-12 col-md-6 col-xl-4 <?= p3($unitsInRegions) ?>">
+<?php if (!$apparitions->isEmpty()): ?>
+	<h5>Einheiten in der Region</h5>
+	<br>
+	<div class="container-fluid">
+		<div class="row">
+			<?php foreach ($apparitions as $unit /* @var Unit $unit */): ?>
+				<div class="col-12 col-md-6 col-xl-4 <?= p3(++$i) ?>">
 					<?= $this->template('unit', $unit) ?>
 				</div>
-			</div>
+			<?php endforeach ?>
 		</div>
-	<?php endif ?>
-<?php endforeach ?>
+	</div>
+<?php endif ?>
