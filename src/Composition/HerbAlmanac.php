1754b95ae6a1754ed9de51be39b4c36c8b52ad26
lemuria-pbem <lemuria@online.de>
2023-03-04 12:23:56 +0100

    [FEATURE] Print herb almanac in sorted order.

--- src/Composition/HerbAlmanac.php
+++ src/Composition/HerbAlmanac.php
@@ -4,31 +4,21 @@ namespace Lemuria\Renderer\Text\Composition;
 
 use function Lemuria\Renderer\Text\View\center;
 use function Lemuria\Renderer\Text\View\hr;
-use Lemuria\Engine\Fantasya\Command\Explore;
-use Lemuria\Engine\Fantasya\Event\Visit;
-use Lemuria\Lemuria;
-use Lemuria\Model\Dictionary;
 use Lemuria\Model\Fantasya\Composition\HerbAlmanac as Almanac;
+use Lemuria\Renderer\Text\Model\HerbalBookTrait;
 
 final class HerbAlmanac extends AbstractComposition
 {
+	use HerbalBookTrait;
+
 	public function getContent(): string {
 		$herbalBook = $this->getAlmanac()->HerbalBook();
 		if ($herbalBook->isEmpty()) {
 			return $this->noContent('Die Seiten dieses Kräuteralmanachs sind leer.');
 		}
 
-		$content    = PHP_EOL . hr() . PHP_EOL . center('Inhalt') . PHP_EOL;
-		$dictionary = new Dictionary();
-		$round      = Lemuria::Calendar()->Round() - 1;
-		foreach ($herbalBook as $region) {
-			$herbage  = $herbalBook->getHerbage($region);
-			$rounds   = $herbalBook->getVisit($region)->Round() - $round;
-			$content .= $dictionary->get('landscape.' . $region->Landscape()) . ' ' . $region->Name() . ': ';
-			$content .= $dictionary->get('amount.' . Explore::occurrence($herbage)) . ' ' . $dictionary->get('resource.' . $herbage->Herb(), 1) . ' ';
-			$content .= '(' . str_replace('$rounds', (string)abs($rounds), $dictionary->get('visit.' . Visit::when($rounds)) ). ')';
-			$content .= PHP_EOL;
-		}
+		$content  = PHP_EOL . hr() . PHP_EOL . center('Inhalt') . PHP_EOL;
+		$content .= $this->sortAndGenerate($herbalBook);
 		return $content;
 	}
 
---
+++ src/Model/HerbalBookTrait.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types = 1);
+namespace Lemuria\Renderer\Text\Model;
+
+use function Lemuria\Renderer\Text\View\underline;
+use Lemuria\Engine\Fantasya\Command\Explore;
+use Lemuria\Engine\Fantasya\Event\Visit;
+use Lemuria\Id;
+use Lemuria\Lemuria;
+use Lemuria\Model\Dictionary;
+use Lemuria\Model\Fantasya\Continent;
+use Lemuria\Model\Fantasya\HerbalBook;
+use Lemuria\Model\Fantasya\Region;
+use Lemuria\Model\Fantasya\World\FantasyaAtlas;
+use Lemuria\SortMode;
+
+trait HerbalBookTrait
+{
+	protected function sortAndGenerate(HerbalBook $herbalBook): string {
+		$output     = '';
+		$round      = Lemuria::Calendar()->Round() - 1;
+		$dictionary = new Dictionary();
+		$continents = $this->getContinents($herbalBook);
+		foreach ($continents as $id => $atlas) {
+			if (!$atlas->isEmpty()) {
+				$continent = Continent::get(new Id($id));
+				if (!empty($output)) {
+					$output .= PHP_EOL;
+				}
+				$output .= underline('Kräutervorkommen auf ' . $continent->Name());
+
+				$landscapes = $this->sortByLandscape($atlas, $dictionary);
+				foreach ($landscapes as $regions) {
+					$output .= PHP_EOL;
+					foreach ($regions as $region) {
+						$herbage = $herbalBook->getHerbage($region);
+						$rounds  = $herbalBook->getVisit($region)->Round() - $round;
+
+						$output .= $dictionary->get('landscape.' . $region->Landscape()) . ' ' . $region->Name() . ': ';
+						$output .= $dictionary->get('amount.' . Explore::occurrence($herbage)) . ' ' . $dictionary->get('resource.' . $herbage->Herb(), 1) . ' ';
+						$output .= '(' . str_replace('$rounds', (string)abs($rounds), $dictionary->get('visit.' . Visit::when($rounds)) ). ')';
+						$output .= PHP_EOL;
+					}
+				}
+			}
+		}
+		return $output;
+	}
+
+	/**
+	 * @return array<int, FantasyaAtlas>
+	 */
+	private function getContinents(HerbalBook $herbalBook): array {
+		$continents = [];
+		foreach (Continent::all() as $continent) {
+			$atlas = new FantasyaAtlas();
+			$atlas->forContinent($continent);
+			foreach ($herbalBook as $region) {
+				$atlas->add($region);
+			}
+			$atlas->sort(SortMode::NorthToSouth);
+			$continents[$continent->Id()->Id()] = $atlas;
+		}
+		return $continents;
+	}
+
+	/**
+	 * @return array<string, array<Region>>
+	 */
+	private function sortByLandscape(FantasyaAtlas $atlas, Dictionary $dictionary): array {
+		$landscapes = [];
+		foreach ($atlas as $region) {
+			$landscape = $dictionary->get('landscape.' . $region->Landscape());
+			if (!isset($landscapes[$landscape])) {
+				$landscapes[$landscape] = [];
+			}
+			$landscapes[$landscape][] = $region;
+		}
+		ksort($landscapes);
+		return $landscapes;
+	}
+}
