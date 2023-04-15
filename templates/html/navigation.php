0d5061a6d1867bcc796521f8a2d2314da9567278
lemuria-pbem <lemuria@online.de>
2023-04-15 14:35:24 +0200

    [BUGFIX] Fix navigation bar layout.

--- templates/html/navigation.php
+++ templates/html/navigation.php
@@ -19,7 +19,7 @@ $travelLog = $this->travelLog;
 <div class="fixed-top vh-100">
 	<a id="navbar-toggle" class="btn btn-small btn-light fixed-top" data-bs-toggle="collapse" href="#navbar" role="button" aria-expanded="true" aria-controls="navbar" title="Taste: I">Inhalt</a>
 	<a id="toggle-map" class="btn btn-small btn-light fixed-bottom" data-bs-toggle="modal" data-bs-target="#modal-map" title="Taste: W">Karte</a>
-	<nav id="navbar" class="navbar navbar-light bg-light collapse">
+	<nav id="navbar" class="navbar navbar-light bg-light collapse pt-0">
 		<nav class="nav nav-pills flex-column">
 			<?php foreach ($travelLog as $continent => $atlas): ?>
 				<?php if ($atlas->count() > 0): ?>
@@ -28,13 +28,13 @@ $travelLog = $this->travelLog;
 						<?php if ($census->has($region->Id())): ?>
 							<?php if ($region->Landscape() instanceof Navigable): ?>
 								<?php if ($isMonster || !$region->Fleet()->isEmpty()): ?>
-									<a class="navigable nav-link" href="#<?= id($region) ?>"><?= $region->Name() ?></a>
+									<a class="navigable nav-link pb-0" href="#<?= id($region) ?>"><?= $region->Name() ?></a>
 								<?php endif ?>
 								<?php foreach (View::sortedFleet($region) as $vessel): ?>
 									<a class="vessel nav-link ml-3 py-0" href="#<?= id($vessel) ?>"><?= $vessel->Name() ?></a>
 								<?php endforeach ?>
 							<?php else: ?>
-								<a class="location <?= strtolower(getClass($region->Landscape())) ?> nav-link" href="#<?= id($region) ?>"><?= $region->Name() ?></a>
+								<a class="location <?= strtolower(getClass($region->Landscape())) ?> nav-link pb-0" href="#<?= id($region) ?>"><?= $region->Name() ?></a>
 								<?php if (!$region->Estate()->isEmpty() || !$region->Fleet()->isEmpty()): ?>
 									<nav id="nav-<?= id($region) ?>" class="nav nav-pills flex-column">
 										<?php foreach (View::sortedEstate($region) as $construction): ?>
