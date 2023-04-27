2fab9ebf185d8a4f7f9b7dbc25758b8c45671a50
lemuria-pbem <lemuria@online.de>
2023-04-27 18:55:38 +0200

    [FEATURE] Link from hostilities to the region of a battle.

--- templates/html/hostilities.php
+++ templates/html/hostilities.php
@@ -1,12 +1,14 @@
 <?php
 declare (strict_types = 1);
 
+use function Lemuria\Renderer\Text\View\id;
 use Lemuria\Model\Fantasya\Region;
 use Lemuria\Renderer\Text\View\Html;
 
 /** @var Html $this */
 
 $hostilities = [];
+$locations   = [];
 $links       = [];
 $i           = 0;
 foreach ($this->hostilities() as $battle):
@@ -20,6 +22,7 @@ foreach ($this->hostilities() as $battle):
 	$name     = $location->Name() . ' <span class="badge text-bg-secondary font-monospace">' . $location->Id() . '</span>';
 
 	$hostilities[$name] = $this->toAndString($participants);
+	$locations[$name]   = $location;
 	$links[]            = $this->battleLogPath($battle);
 endforeach;
 
@@ -29,9 +32,9 @@ endforeach;
 		<h3>Kampfberichte</h3>
 
 		<?php foreach ($hostilities as $location => $participants): ?>
-		In <?= $location ?> gab es einen Kampf zwischen den Parteien <?= $participants ?>.
-		<a href="<?= $links[$i++] ?>">Kampfbericht anzeigen</a>
-		<br>
+			In <a href="#<?= id($locations[$location]) ?>"><?= $location ?></a> gab es einen Kampf zwischen den Parteien <?= $participants ?>.
+			<a href="<?= $links[$i++] ?>">Kampfbericht anzeigen</a>
+			<br>
 		<?php endforeach ?>
 	</div>
 <?php endif ?>
