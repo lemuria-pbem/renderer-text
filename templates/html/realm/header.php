4f9f0f584489de251a1a46fa998f14ec8c509fcc
lemuria-pbem <lemuria@online.de>
2023-08-20 18:08:53 +0200

    [FEATURE] Show realm identifier in realm header.

--- templates/html/realm/header.php
+++ templates/html/realm/header.php
@@ -19,13 +19,13 @@ $isCentral = $region === $central;
 <?php if ($realm): ?>
 	<?php if ($isCentral): ?>
 		<p>
-			Zentralregion des Reiches <?= $realm->Name() ?>. Maximale Transportkapazität: <?= number(State::getInstance()->getRealmFleet($realm)->Incoming()) ?> GE.
+			Zentralregion des Reiches <?= $realm->Name() ?> <span class="badge text-bg-light font-monospace"><?= $realm->Identifier() ?></span>. Maximale Transportkapazität: <?= number(State::getInstance()->getRealmFleet($realm)->Incoming()) ?> GE.
 			<?= $this->template('description', $realm) ?>
 		</p>
 	<?php else: ?>
 		<p>
 			Die Region gehört zum Reich
-			<a href="#<?= id($central) ?>"><?= $realm->Name() ?></a>.
+			<a href="#<?= id($central) ?>"><?= $realm->Name() ?></a> <span class="badge text-bg-light font-monospace"><?= $realm->Identifier() ?></span>.
 		</p>
 	<?php endif ?>
 <?php endif ?>
